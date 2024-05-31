c649a23ff47ad600464a0077886e86d3189d5061
RodrigoJV3 <rodrigo_201926059@test.edu.mx>
2024-05-31 15:11:38 -0600

    new xacro file

--- src/robot_pkg/launch/display.launch.py
+++ src/robot_pkg/launch/display.launch.py
@@ -1,5 +1,5 @@
 import launch
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import command, LaunchConfiguration
 import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
