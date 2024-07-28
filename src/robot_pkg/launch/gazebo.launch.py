b8b3818785718e55c75ed4994c5151bae337196f
RodrigoJV3 <rodrigo_201926059@test.edu.mx>
2024-07-27 18:35:35 -0600

    control swap

--- src/robot_pkg/launch/gazebo.launch.py
+++ src/robot_pkg/launch/gazebo.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
@@ -9,12 +10,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     package_name='robot_pkg'
-
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
     # RSP
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': use_ros2_control}.items()
     )
 
     # joystick = IncludeLaunchDescription(
