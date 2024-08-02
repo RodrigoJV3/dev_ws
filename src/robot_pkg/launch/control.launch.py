313d539c99296c0c267a68e2fe9f4cdae7c4669f
RodrigoJV3 <rodrigo_201926059@test.edu.mx>
2024-08-02 17:45:28 -0600

    slamtbox config good

--- src/robot_pkg/launch/control.launch.py
+++ src/robot_pkg/launch/control.launch.py
@@ -1,15 +1,11 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-
 from launch_ros.actions import Node
 
 
