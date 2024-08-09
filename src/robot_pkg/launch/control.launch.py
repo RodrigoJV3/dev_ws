d539e1eb5406c234c123178a2211f2ac588016ab
RodrigoJV3 <rodrigo_201926059@test.edu.mx>
2024-08-09 11:16:34 -0600

    some launch files

--- src/robot_pkg/launch/control.launch.py
+++ src/robot_pkg/launch/control.launch.py
@@ -36,13 +36,13 @@ def generate_launch_description():
     # )
 
 
-    # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    # twist_mux = Node(
-    #         package="twist_mux",
-    #         executable="twist_mux",
-    #         parameters=[twist_mux_params],
-    #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-    #     )
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
 
     
 
@@ -99,7 +99,7 @@ def generate_launch_description():
         rsp,
         joint_state_publisher,
         #joystick,
-        #twist_mux,
+        twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
